f684fd457ed28aeea8f7e1a0e09edcef084230ba
Marjon Cajocon <88709877+marjoncajocon@users.noreply.github.com>
2024-03-12 15:06:53 +0800

    Update n2word.py

--- python/date/n2word.py
+++ python/date/n2word.py
@@ -1,14 +1,19 @@
+# recursive function
 def int_to_word(num: int):
     unit1 = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
     unit2 = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
     unit3 = [
-        "Trillion", "Billion", "Million", "Thousand", "hundred"
+        "Quadrillion,", "Trillion,", "Billion,", "Million,", "Thousand,", "hundred"
     ]
-    unit3_val = [1000000000000, 1000000000, 1000000, 1000, 100]
+    unit3_val = [1000000000000000, 1000000000000, 1000000000, 1000000, 1000, 100]
+
     
     ret = ''
     
-    if num < 20:
+
+    if num <= 0:
+        return ""
+    elif num < 20:
         ret = unit1[num]
     elif num < 100:
         ret += str(unit2[num // 10]) + " " + str(unit1[num % 10])
@@ -25,4 +30,5 @@ def int_to_word(num: int):
 
     return ret
     
-print(int_to_word(2000123));
+    
+print(int_to_word(9999999999999));
