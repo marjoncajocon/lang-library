344d288ad669398f880841bbce0be8559ff575cd
Marjon Cajocon <88709877+marjoncajocon@users.noreply.github.com>
2025-02-19 09:37:24 +0800

    changing from slice to maps,,, and we can get the O(1) performance

--- session.go
+++ session.go
@@ -1,47 +1,64 @@
+// / revision for the session storage and we will using maps for the sessions
 package lib
 
 import (
 	"fmt"
 	"math/rand/v2"
+	"sync"
 )
 
-type Kv struct {
-	Key   string
-	Value string
-}
+var mutex sync.Mutex // im using this to avoid datarace
+// where using mutex, since we are using session ram as a temporary storage session
+// this is a secure session but, it may not good for large user or about millions user,,
+// in the future development for this session we will create a sqlite3 as the storage of the session
 
 type Session struct {
 	Id   string
-	Data []Kv
+	Data map[string]string
 }
 
 // where are using Ram to access the session
-var SessionStorage = []*Session{}
+//var SessionStorage = []*Session{}
+
+var SessionStorage = make(map[string]Session)
 
 func GetSession(key string) *Session {
-	for _, x := range SessionStorage {
-		if x.Id == key {
-			return x
-		}
+	mutex.Lock()
+	defer mutex.Unlock()
+	// for _, x := range SessionStorage {
+	// 	if x.Id == key {
+	// 		return x
+	// 	}
+	// }
+	if _, ok := SessionStorage[key]; ok {
+		ses := SessionStorage[key]
+		return &ses
 	}
 
 	return nil
 }
 
 func DeleteSession(key string) {
-	for index, x := range SessionStorage {
-		if x.Id == key {
-			SessionStorage = append(SessionStorage[:index], SessionStorage[index+1:]...)
-		}
-	}
-
+	mutex.Lock()
+	defer mutex.Unlock()
+	// for index, x := range SessionStorage {
+	// 	if x.Id == key {
+	// 		SessionStorage = append(SessionStorage[:index], SessionStorage[index+1:]...)
+	// 		break
+	// 	}
+	// }
+	delete(SessionStorage, key)
 }
 
-func GetAllSession() []*Session {
+func GetAllSession() map[string]Session {
+	mutex.Lock()
+	defer mutex.Unlock()
 	return SessionStorage
 }
 
 func (s *Session) New(key string) string {
+	s.Data = make(map[string]string) // initialized the data
+
 	s.Id = ""
 	if key == "" {
 		// create a niew key
@@ -63,11 +80,16 @@ func (s *Session) New(key string) string {
 
 	} else {
 
-		for _, x := range SessionStorage {
-			if s.Id == x.Id {
-				s.Id = key
-				break
-			}
+		// for _, x := range SessionStorage {
+		// 	if s.Id == x.Id {
+		// 		s.Id = key
+		// 		break
+		// 	}
+		// }
+		mutex.Lock()
+		defer mutex.Unlock()
+		if _, ok := SessionStorage[key]; ok {
+			s.Id = key
 		}
 
 	}
@@ -76,27 +98,55 @@ func (s *Session) New(key string) string {
 }
 
 func (s *Session) GetKey() string {
-
+	mutex.Lock()
+	defer mutex.Unlock()
 	return s.Id
 }
 
-func (s *Session) Set(key string, value string) *Session {
-	data := Kv{Key: key, Value: value}
+func (s *Session) Set(key string, value string) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	// data := Kv{Key: key, Value: value}
+	// s.Data = append(s.Data, data)
 
-	s.Data = append(s.Data, data)
-	return s
+	s.Data[key] = value /// set value
+}
+
+func (s *Session) UnSet(key string) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	// for index, item := range s.Data {
+	// 	if item.Key == key {
+	// 		s.Data = append(s.Data[:index], s.Data[index+1:]...)
+	// 		break
+	// 	}
+	// }
+
+	delete(s.Data, key)
+
 }
 
 func (s *Session) Get(key string) string {
-	for _, x := range s.Data {
-		if x.Key == key {
-			return x.Value
-		}
-	}
-	return ""
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	// for _, x := range s.Data {
+	// 	if x.Key == key {
+	// 		return x.Value
+	// 	}
+	// }
+	// return ""
+
+	return s.Data[key]
 }
 
 func (s *Session) Store() {
 	// call the global variable to store the new session
-	SessionStorage = append(SessionStorage, s)
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	//SessionStorage = append(SessionStorage, s)
+
+	SessionStorage[s.Id] = Session{Id: s.Id, Data: s.Data}
 }
