203a65e3775ca1987dafb4be6a2f562abd963d73
Marjon Cajocon <88709877+marjoncajocon@users.noreply.github.com>
2025-05-22 13:12:34 +0800

    Update semaphore.go

--- semaphore.go
+++ semaphore.go
@@ -31,59 +31,34 @@ wg.Wait()
 package main
 
 import (
-	"fmt"
-	"sync"
-	"time"
+    "fmt"
+    "sync"
 )
 
-// Semaphore controls access to a limited number of resources.
-type Semaphore struct {
-	sem chan struct{}
-}
+func worker(id int, sem chan struct{}, wg *sync.WaitGroup) {
+    defer wg.Done()
 
-// NewSemaphore creates a new semaphore with a given capacity.
-func NewSemaphore(capacity int) *Semaphore {
-	return &Semaphore{
-		sem: make(chan struct{}, capacity),
-	}
-}
+    // Acquire semaphore
+    sem <- struct{}{}
+    fmt.Printf("Worker %d started\n", id)
 
-// Acquire acquires a semaphore permit, blocking if necessary.
-func (s *Semaphore) Acquire() {
-	s.sem <- struct{}{}
-}
+    // Do some work
+    // ...
 
-// Release releases a semaphore permit.
-func (s *Semaphore) Release() {
-	<-s.sem
+    // Release semaphore
+    <-sem
+    fmt.Printf("Worker %d finished\n", id)
 }
 
 func main() {
-	// Number of items to process
-	numItems := 20
+    maxConcurrency := 3
+    sem := make(chan struct{}, maxConcurrency)
+    var wg sync.WaitGroup
 
-	// Batch size (number of concurrent workers)
-	batchSize := 5
+    for i := 1; i <= 10; i++ {
+        wg.Add(1)
+        go worker(i, sem, &wg)
+    }
 
-	// Create a semaphore with the batch size as capacity
-	sem := NewSemaphore(batchSize)
-
-	var wg sync.WaitGroup
-
-	// Simulate processing items in batches
-	for i := 0; i < numItems; i++ {
-		wg.Add(1)
-		sem.Acquire() // Acquire a permit before starting a worker
-		go func(item int) {
-			defer wg.Done()
-			defer sem.Release() // Release the permit when worker is done
-			
-			// Simulate work
-			fmt.Printf("Processing item: %d\n", item)
-			time.Sleep(100 * time.Millisecond)
-		}(i)
-	}
-
-	wg.Wait()
-	fmt.Println("All items processed.")
+    wg.Wait()
 }
