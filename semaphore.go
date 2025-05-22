bb0b009cd08d8e27136462d8238069dd290a77c2
Marjon Cajocon <88709877+marjoncajocon@users.noreply.github.com>
2025-05-22 11:53:10 +0800

    Create semaphore.go

---
+++ semaphore.go
@@ -0,0 +1,22 @@
+/// batch processing in go lang
+
+sem := make(chan struct{}, 1)
+	var wg sync.WaitGroup
+	for _, file := range files {
+		wg.Add(1)
+		//fmt.Println(file)
+		sem <- struct{}{}
+		go func() {
+			defer wg.Done()
+			defer func() { <-sem }()
+
+			cmd := exec.Command("tesseract", file, "-")
+			scan, err := cmd.CombinedOutput()
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println(string(scan))
+
+		}()
+	}
+	wg.Wait()
