bba7ff39f97a27925e29b6d03c9dba8822601eb7
Marjon Cajocon <88709877+marjoncajocon@users.noreply.github.com>
2025-05-22 11:53:59 +0800

    Update semaphore.go

--- semaphore.go
+++ semaphore.go
@@ -1,22 +1,23 @@
 /// batch processing in go lang
+var files []string // example list file path
 
 sem := make(chan struct{}, 1)
-	var wg sync.WaitGroup
-	for _, file := range files {
-		wg.Add(1)
-		//fmt.Println(file)
-		sem <- struct{}{}
-		go func() {
-			defer wg.Done()
-			defer func() { <-sem }()
+var wg sync.WaitGroup
+for _, file := range files {
+	wg.Add(1)
+	//fmt.Println(file)
+	sem <- struct{}{}
+	go func() {
+		defer wg.Done()
+		defer func() { <-sem }()
 
-			cmd := exec.Command("tesseract", file, "-")
-			scan, err := cmd.CombinedOutput()
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println(string(scan))
+		cmd := exec.Command("tesseract", file, "-")
+		scan, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(scan))
 
-		}()
-	}
-	wg.Wait()
+	}()
+}
+wg.Wait()
