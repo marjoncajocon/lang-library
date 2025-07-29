b40da6ea1c50ed16b6cc8d74721e95f5734b7406
Marjon Cajocon <88709877+marjoncajocon@users.noreply.github.com>
2025-07-29 10:52:10 +0800

    Maybe it is optimized, because  it never call or re create the unit1 upto unit3 in toWord, it call once if the create an instance of struct

--- n2word.go
+++ n2word.go
@@ -6,10 +6,21 @@ import (
 )
 
 type Number struct {
+	Unit1         [21]string
+	Unit2         [10]string
+	Unit3         [6]string
+	Unit3_val     [6]int
+	Unit3_val_len int
 }
 
 func NewNumber() *Number {
-	return &Number{}
+	return &Number{
+		Unit1:         [21]string{"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"},
+		Unit2:         [10]string{"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"},
+		Unit3:         [6]string{"Quadrillion", "Trillion", "Billion", "Million", "Thousand", "hundred"},
+		Unit3_val:     [6]int{1000000000000000, 1000000000000, 1000000000, 1000000, 1000, 100},
+		Unit3_val_len: 6,
+	}
 }
 func (obj *Number) Int2Word(num int) string {
 	// trim character
@@ -18,26 +29,27 @@ func (obj *Number) Int2Word(num int) string {
 
 func (obj *Number) toWord(num int) string {
 
-	unit1 := [...]string{"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"}
-	unit2 := [...]string{"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"}
-	unit3 := [...]string{"Quadrillion", "Trillion", "Billion", "Million", "Thousand", "hundred"}
-	unit3_val := [...]int{1000000000000000, 1000000000000, 1000000000, 1000000, 1000, 100}
+	/*
+		unit1 := [...]string{"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"}
+		unit2 := [...]string{"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"}
+		unit3 := [...]string{"Quadrillion", "Trillion", "Billion", "Million", "Thousand", "hundred"}
+		unit3_val := [...]int{1000000000000000, 1000000000000, 1000000000, 1000000, 1000, 100}
+	*/
 
 	ret := ""
 	if num < 0 {
 		return ""
 	} else if num < 20 {
-		ret = unit1[num]
+		ret = obj.Unit1[num]
 	} else if num < 100 {
-		ret += unit2[num/10] + " " + unit1[num%10]
+		ret += obj.Unit2[num/10] + " " + obj.Unit1[num%10]
 	} else {
 		i := 0
-		unit3_len := len(unit3_val)
-		for i < unit3_len {
-			d := num / unit3_val[i]
-			r := num % unit3_val[i]
+		for i < obj.Unit3_val_len {
+			d := num / obj.Unit3_val[i]
+			r := num % obj.Unit3_val[i]
 			if d > 0 {
-				ret += obj.toWord(d) + " " + unit3[i] + " " + obj.toWord(r)
+				ret += obj.toWord(d) + " " + obj.Unit3[i] + " " + obj.toWord(r)
 				break
 			}
 			i++
@@ -50,6 +62,6 @@ func (obj *Number) toWord(num int) string {
 
 func main() {
 	n := NewNumber()
-	fmt.Println(n.Int2Word(143300))
+	fmt.Println(n.Int2Word(999999))
 	//fmt.Println(n.Int2Word(30))
 }
